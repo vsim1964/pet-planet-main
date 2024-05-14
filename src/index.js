15306d2b7a97c60401eb84460953e3a9142bfea0
setkanet <128897001+setkanet@users.noreply.github.com>
2024-05-14 22:42:17 +0300

    Lesson 1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.store__category-button');
+
+const changeActiveBtn = (evt) => {
+	const target = evt.target;
+	buttons.forEach((button) => {
+		button.classList.remove('store__category-button-active');
+	})
+	target.classList.add('store__category-button-active');
+}
+
+buttons.forEach((button) => {
+	button.addEventListener('click', changeActiveBtn);
+})
